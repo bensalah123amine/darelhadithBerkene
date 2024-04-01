d63e1cc5812fe3edd6421806aff174b3f0130539
bensalah123amine <bensalahamine62@gmail.com>
2024-04-01 13:00:21 +0200

    add tittle wep site9

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    distDir:'dist'
-   
+    pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+	output: "export", 
 };
 
   
