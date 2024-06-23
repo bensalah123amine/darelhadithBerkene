ceb85bcd56140d47c62b0036dcfae1d16f5a8b0f
bensalah123amine <115670104+bensalah123amine@users.noreply.github.com>
2024-06-23 18:46:16 +0100

    up page.tsx

--- app/anchita/page.tsx
+++ app/anchita/page.tsx
@@ -31,7 +31,7 @@ const Anchita = () => {
         <h1 className='text-bgTitle'>{item.title}</h1>
         <p>{item.description}</p>
         <div className='h-auto'> 
-          <img
+          <Image
             src={item.url}
             width={400}
             height={200}
